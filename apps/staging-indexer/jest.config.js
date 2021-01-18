d516e42adc47d20687a0345f4e7bb4945db5f9e3
Matías Barrios <matias.barriosn@gmail.com>
2021-01-18 12:09:13 -0300

    initial commit

---
+++ apps/staging-indexer/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'staging-indexer',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/staging-indexer',
+};
