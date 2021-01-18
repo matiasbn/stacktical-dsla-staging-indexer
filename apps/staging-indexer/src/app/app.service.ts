3610fb0d1fb4ca75e2e04b036ee313f9d8431682
Matías Barrios <matias.barriosn@gmail.com>
2021-01-18 14:01:14 -0300

    fix: response body updated

--- apps/staging-indexer/src/app/app.service.ts
+++ apps/staging-indexer/src/app/app.service.ts
@@ -3,8 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData() {
+    const efficiency = 100 - Math.random() * 10;
+    const validations = Math.floor(10000 * Math.random());
+    const hits = Math.floor((validations * efficiency) / 100);
+    const misses = validations - hits;
+    console.log(hits);
+    console.log(misses);
     return {
-      data: { getSLI: 100 - Math.random() * 10 },
+      data: { validations, hits, misses },
     };
   }
 }
